fc183cc5fd16a1decc084e2878d5e770cdb45430
marvelousko <muhammadsyiraj12@gmail.com>
2024-06-22 00:15:20 +0800

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 // trigger to play music in the background with sweetalert
 window.addEventListener('load', () => {
     Swal.fire({
-        title: 'mau pake backsound?',
+        title: 'mau pake backsound ?',
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
